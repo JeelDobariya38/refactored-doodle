4cd9f2f1b26e4534fdc95b4463d0df3950022a8d
Jeel Dobariya <136002730+JeelDobariya38@users.noreply.github.com>
2026-01-03 21:11:26 +0530

    update

---
+++ q2.py
@@ -0,0 +1,49 @@
+"""
+# 🌱 Question 2 — Listening and replying
+
+Write a program that:
+- asks the user for their favorite color using input
+- stores it in a variable
+- prints a sentence using that value
+
+```md
+Your favorite color is blue
+```
+
+
+
+## Understanding
+
+...
+
+
+
+## Edge Cases
+
+...
+
+
+
+## Brute Force Approach
+
+Time:  O(?) ->  ≈? ms
+Space: O(?) ->  ≈? bits
+
+```python
+
+```
+
+
+
+## Optimized Solution
+
+...
+
+Time:  O(?) ->  ≈? ms
+Space: O(?) ->  ≈? bits
+
+```python
+
+```
+
+"""
